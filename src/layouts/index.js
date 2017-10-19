7a2d138efb861e2f50a7c5813811490a8a3caf81
Sachin <sachin.nicky.com>
2017-10-20 02:17:17 +0530

    gatsby initial

---
+++ src/layouts/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+import favicon from "../favicon.png";
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Sachin</title>
+      <link rel="shortcut icon" type="image/png" href={favicon} />
+      />
+    </Helmet>
+
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+};
+
+export default TemplateWrapper;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const IndexPage = () => (
+  <div style={{ margin: "3rem auto", maxWidth: 600 }}>
+    <h1>Sachin Maharana.</h1>
+    <p>Welcome to sachinmaharana.me</p>
+    <p>Something exciting is on its way.</p>
+  </div>
+);
+
+export default IndexPage;
