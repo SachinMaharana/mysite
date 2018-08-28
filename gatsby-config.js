810ddff2c102a75c7e62071be634dac2c5956f2d
SachinMaharana <sachin.nicky@gmail.com>
2018-08-29 03:19:24 +0530

    an

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,20 @@ module.exports = {
   siteMetadata: {
     title: `Sachin`
   },
-  plugins: [
+  plugins: [{
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-124814241-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
