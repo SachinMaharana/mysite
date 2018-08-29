ddad370c703e9639aa4805dd6507b58f994915b8
SachinMaharana <sachin.nicky@gmail.com>
2018-08-29 21:41:23 +0530

    time

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -62,7 +62,7 @@ const TemplateWrapper = ({ children }) => (
             <Typist
               avgTypingSpeed={80}
               cursor={{ hideWhenDone: true, blink: true }}
-              startDelay={1600}
+              startDelay={1000}
             >
               {/* son
               <Typist.Delay ms={500} />
